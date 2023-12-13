ef6d977b36f27601bc3255d96cc72774494ea041
Jonathan Travaglia <jonathan.travaglia@gmail.com>
2023-12-13 12:09:45 +1030

    Clean day 12

--- src/P12.hs
+++ src/P12.hs
@@ -21,8 +21,11 @@ parse :: String -> [Input]
 parse = map (parseLine . words) . lines
 
 parseLine :: [String] -> Input
-parseLine (springsStr:blockStr:_) = Input springsStr (map read (splitOn "," blockStr))
+parseLine (springsStr:blockStr:_) = Input springsStr (parseBlocks blockStr)
 parseLine _ = error "bad input"
+
+parseBlocks :: String -> [Int]
+parseBlocks = map read . splitOn ","
 
 solve1 :: [Input] -> Int
 solve1 = sum . map (fst . solutions)
@@ -31,7 +34,13 @@ solve2 :: [Input] -> Int
 solve2 = sum . map (fst . solutions . unfoldRecord)
 
 unfoldRecord :: Input -> Input
-unfoldRecord (Input pattern blocks) = Input (intercalate "?" (replicate 5 pattern)) (blocks ++ blocks ++ blocks ++ blocks ++ blocks)
+unfoldRecord (Input pattern blocks) = Input (unfoldPattern pattern) (unfoldBlocks blocks)
+
+unfoldPattern :: String -> String
+unfoldPattern = intercalate "?" . replicate 5
+
+unfoldBlocks :: [Int] -> [Int]
+unfoldBlocks = concat . replicate 5
 
 solutions :: Input -> (Int, Cache)
 solutions (Input springs blocks) = countSolutions M.empty springs blocks
@@ -43,12 +52,21 @@ countSolutions cache pattern []
 countSolutions cache [] _ = (0, cache)
 countSolutions cache ('.':pattern) blocks = countSolutions cache pattern blocks
 countSolutions cache ('#':pattern) (nextBlock:blocks) = placeBlock cache pattern (nextBlock-1) blocks
-countSolutions cache (_:pattern) blocks@(nextBlock:remBlocks) = case M.lookup (pattern, blocks) cache of
+countSolutions cache (_:pattern) blocks@(nextBlock:remBlocks) = memoize cache (pattern, blocks) (processQ cache pattern blocks nextBlock remBlocks)
+
+memoize :: Cache -> (String, [Int]) -> (Int, Cache) -> (Int, Cache)
+memoize cache key f = 
+    case M.lookup key cache of
         Just x -> (x, cache)
         Nothing ->
-            let (count1, cache1) = placeBlock cache pattern (nextBlock-1) remBlocks 
-                (count2, cache2) = countSolutions cache1 pattern blocks
-            in  (count1 + count2, M.insert (pattern, blocks) (count1 + count2) cache2)
+            let (result, cache') = f
+            in  (result, M.insert key result cache')
+
+processQ :: Cache -> String -> [Int] -> Int -> [Int] -> (Int, Cache)
+processQ cache pattern blocks nextBlock remBlocks =
+    let (count1, cache1) = placeBlock cache pattern (nextBlock-1) remBlocks 
+        (count2, cache2) = countSolutions cache1 pattern blocks
+    in  (count1 + count2, cache2)
 
 placeBlock :: Cache -> String -> Int -> [Int] -> (Int, Cache)
 placeBlock cache pattern 0 blocks = placeSpace cache pattern blocks
