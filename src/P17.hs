9643ba4783f81fa30d42001a714a8f4578efa93c
Jonathan Travaglia <jonathan.travaglia@gmail.com>
2023-12-21 15:26:17 +1030

    Day 17 performance improvement

--- src/P17.hs
+++ src/P17.hs
@@ -9,6 +9,7 @@ import Data.Maybe (catMaybes)
 import Data.Char (digitToInt)
 import Data.Function (on)
 
+data Orientation = Horizontal | Vertical deriving (Ord, Eq)
 data Direction = DirLeft | DirRight | DirUp | DirDown deriving (Ord, Eq)
 data CurrentNode = CurrentNode Coord Direction Int Int deriving Eq -- location direction momentum heat
 
@@ -28,25 +29,25 @@ parse :: String -> M.Map Coord Int
 parse =  M.map digitToInt . textToCoordMap
 
 solve1 :: M.Map Coord Int -> Int
-solve1 input = dijkstra17 input 1 3 (targetCoord input) (startState $ initState input) ((0,0), DirRight, 0)
+solve1 input = dijkstra17 input 1 3 (targetCoord input) (startState $ initState input) ((0,0), Vertical, 0)
 
 solve2 :: M.Map Coord Int -> Int
-solve2 input = dijkstra17 input 4 7 (targetCoord input) (startState $ initState input) ((0,0), DirRight, 0)
+solve2 input = dijkstra17 input 4 7 (targetCoord input) (startState $ initState input) ((0,0), Vertical, 0)
 
 targetCoord :: M.Map Coord Int -> Coord
 targetCoord input = (maximum (map fst (M.keys input)), maximum (map snd (M.keys input)))
 
-initState :: M.Map Coord Int -> M.Map (Coord, Direction) Int
-initState = M.fromList . concatMap (\loc -> [((loc, direction), maxBound) | direction <- [DirLeft, DirRight, DirUp, DirDown]]) . M.keys
+initState :: M.Map Coord Int -> M.Map (Coord, Orientation) Int
+initState = M.fromList . concatMap (\loc -> [((loc, orientation), maxBound) | orientation <- [Horizontal, Vertical]]) . M.keys
 
-startState :: M.Map (Coord, Direction) Int -> M.Map (Coord, Direction) Int
-startState state = foldl (\state' (k, v) -> M.update (Just . min v) k state') state [(((0,0), DirDown), 0), (((0,0), DirRight), 0)] 
+startState :: M.Map (Coord, Orientation) Int -> M.Map (Coord, Orientation) Int
+startState state = foldl (\state' (k, v) -> M.update (Just . min v) k state') state [(((0,0), Horizontal), 0), (((0,0), Vertical), 0)] 
 
-dijkstra17 :: M.Map Coord Int -> Int -> Int -> Coord -> M.Map (Coord, Direction) Int -> (Coord, Direction, Int) -> Int
-dijkstra17 heatMap minMomentum maxMove target state currentNode@(location, direction, heat)
+dijkstra17 :: M.Map Coord Int -> Int -> Int -> Coord -> M.Map (Coord, Orientation) Int -> (Coord, Orientation, Int) -> Int
+dijkstra17 heatMap minMomentum maxMove target state currentNode@(location, orientation, heat)
     | location == target = heat
     | otherwise =
-        let directions = turnDirs direction
+        let directions = turnDirs orientation
             toUpdate = catMaybes $ concatMap (\direction' -> nextBlocks minMomentum maxMove heatMap location direction' heat) directions
             state' = updateNeighbours state toUpdate
             state'' = markVisited state' currentNode
@@ -59,13 +60,13 @@ toNode :: ((a, b), c) -> (a, b, c)
 toNode ((location, direction), heat) = (location, direction, heat)
 
 markVisited :: (Ord a1, Ord b) => M.Map (a1, b) a2 -> (a1, b, c) -> M.Map (a1, b) a2
-markVisited state (location, direction, _) = M.delete (location, direction) state 
+markVisited state (location, direction, _) = M.delete (location, direction) state
 
-updateNeighbours :: M.Map (Coord, Direction) Int -> [(Coord, Direction, Int)] -> M.Map (Coord, Direction) Int
+updateNeighbours :: M.Map (Coord, Orientation) Int -> [(Coord, Orientation, Int)] -> M.Map (Coord, Orientation) Int
 updateNeighbours = foldl updateNeighbour
 
-updateNeighbour :: (Ord a1, Ord b, Ord a2) => M.Map (a1, b) a2 -> (a1, b, a2) -> M.Map (a1, b) a2
-updateNeighbour state (location, direction, heat) = M.update (Just . min heat) (location, direction) state
+updateNeighbour :: M.Map (Coord, Orientation) Int -> (Coord, Orientation, Int) -> M.Map (Coord, Orientation) Int
+updateNeighbour state (location, orientation, heat) = M.update (Just . min heat) (location, orientation) state
 
 move :: (Num b, Num a) => Direction -> (a, b) -> (a, b)
 move DirDown (x,y) = (x, y+1)
@@ -83,11 +84,18 @@ step heatMap (Just (loc, direction, heat)) =
                 let heat' = heat + newHeat
                 in  Just (loc', direction, heat')
 
-nextBlocks :: (Ord a, Ord b, Num b, Num a, Num c) => Int -> Int -> M.Map (a, b) c -> (a, b) -> Direction -> c -> [Maybe ((a, b), Direction, c)]
-nextBlocks minMomentum maxMove heatMap location direction startHeat = take maxMove $ drop minMomentum $ iterate (step heatMap) (Just (location, direction, startHeat))
+nextBlocks :: (Ord a, Ord b, Num b, Num a, Num c) => Int -> Int -> M.Map (a, b) c -> (a, b) -> Direction -> c -> [Maybe ((a, b), Orientation, c)]
+nextBlocks minMomentum maxMove heatMap location direction startHeat = map convertOrientation $ take maxMove $ drop minMomentum $ iterate (step heatMap) (Just (location, direction, startHeat))
 
-turnDirs :: Direction -> [Direction]
-turnDirs DirUp = [DirLeft, DirRight]
-turnDirs DirDown = [DirLeft, DirRight]
-turnDirs DirLeft = [DirUp, DirDown]
-turnDirs DirRight = [DirUp, DirDown]
+convertOrientation :: Maybe (a, Direction, c) -> Maybe (a, Orientation, c)
+convertOrientation = fmap (\(c, dir, heat) -> (c, dirOrientation dir, heat))
+
+turnDirs :: Orientation -> [Direction]
+turnDirs Horizontal = [DirLeft, DirRight]
+turnDirs Vertical = [DirUp, DirDown]
+
+dirOrientation :: Direction -> Orientation
+dirOrientation DirLeft = Vertical
+dirOrientation DirRight = Vertical
+dirOrientation DirUp = Horizontal
+dirOrientation DirDown = Horizontal
