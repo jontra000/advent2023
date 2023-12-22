cc7355e2bd6d8497a659835cbc7eb823708cad8f
Jonathan Travaglia <jonathan.travaglia@gmail.com>
2023-12-23 09:28:07 +1030

    Clean day 19

--- src/P19.hs
+++ src/P19.hs
@@ -1,12 +1,15 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use tuple-section" #-}
 module P19 (run1, run2, inputLocation) where
 
 import qualified Data.Map as M
 import Data.List.Split (splitOn)
 import Data.List (find)
 
-data Rule = Rule Char Char Int String -- property operator value destination
+data Rule = Rule Char (Int -> Int -> Bool) Int String -- property operator value destination
 data Workflow = Workflow [Rule] String
-data Input = Input (M.Map String Workflow) [M.Map Char Int]
+type Part = M.Map Char Int
+data Input = Input (M.Map String Workflow) [Part]
 
 run1 :: String -> Int
 run1 = solve1 . parse
@@ -18,11 +21,11 @@ inputLocation :: String
 inputLocation = "inputs/input19"
 
 parse :: String -> Input
-parse = parse' . splitOn [""] . lines
+parse = parseBlocks . splitOn [""] . lines
 
-parse' :: [[String]] -> Input
-parse' (workflows:parts:_) = Input (parseWorkflows workflows) (parseParts parts)
-parse' _ = error "bad input"
+parseBlocks :: [[String]] -> Input
+parseBlocks (workflows:parts:_) = Input (parseWorkflows workflows) (parseParts parts)
+parseBlocks _ = error "bad input"
 
 parseWorkflows :: [String] -> M.Map String Workflow
 parseWorkflows = M.fromList . map parseWorkflow
@@ -41,13 +44,18 @@ parseRules' ruleStrs = Workflow (map parseRule (init ruleStrs)) (last ruleStrs)
 parseRule :: String -> Rule
 parseRule (property:operator:ruleStr) =
     let (valStr, res) = break (==':') ruleStr
-    in  Rule property operator (read valStr) (tail res)
+    in  Rule property (parseOperator operator) (read valStr) (tail res)
 parseRule _ = error "bad rule"
 
-parseParts :: [String] -> [M.Map Char Int]
+parseOperator :: Ord a => Char -> a -> a -> Bool
+parseOperator '>' = (>)
+parseOperator '<' = (<)
+parseOperator _ = error "bad operator"
+
+parseParts :: [String] -> [Part]
 parseParts = map parsePart
 
-parsePart :: String -> M.Map Char Int
+parsePart :: String -> Part
 parsePart = M.fromList . map parseProperty . splitOn "," . tail . init
 
 parseProperty :: String -> (Char, Int)
@@ -55,73 +63,58 @@ parseProperty (p:_:valStr) = (p, read valStr)
 parseProperty _ = error "bad property"
 
 solve1 :: Input -> Int
-solve1 (Input workflows parts) = sum $ map ratings $ filter (isAccepted workflows) parts
+solve1 = sum . map ratings . acceptedParts
 
-isAccepted :: M.Map String Workflow -> M.Map Char Int -> Bool
+acceptedParts :: Input -> [Part]
+acceptedParts (Input workflows parts) = filter (isAccepted workflows) parts
+
+isAccepted :: M.Map String Workflow -> Part -> Bool
 isAccepted workflows = (=="A") . applyWorkflows workflows "in"
 
-applyWorkflows :: M.Map String Workflow -> String -> M.Map Char Int -> String
-applyWorkflows workflows workflowName part =
-    case M.lookup workflowName workflows of
-        Nothing -> workflowName
-        Just nextWorkflow ->
-            let next = applyWorkflow nextWorkflow part
-            in  applyWorkflows workflows next part
+applyWorkflows :: M.Map String Workflow -> String -> Part -> String
+applyWorkflows workflows workflowName part = maybe workflowName (applyWorkflows' workflows part) (M.lookup workflowName workflows)
 
-applyWorkflow :: Workflow -> M.Map Char Int -> String
-applyWorkflow (Workflow rules defaultDestination) part =
-    case find (ruleMatches part) rules of
-        Nothing -> defaultDestination
-        Just (Rule _ _ _ destination) -> destination
+applyWorkflows' :: M.Map String Workflow -> Part -> Workflow -> String
+applyWorkflows' workflows part nextWorkflow =
+    let next = applyWorkflow nextWorkflow part
+    in  applyWorkflows workflows next part
 
-ruleMatches :: M.Map Char Int -> Rule -> Bool
-ruleMatches part (Rule property '>' value _) =
-    part M.! property > value
-ruleMatches part (Rule property '<' value _) =
-    part M.! property < value
-ruleMatches _ _ = error "bad operator"
+applyWorkflow :: Workflow -> Part -> String
+applyWorkflow (Workflow rules defaultDestination) part = maybe defaultDestination getDestination (find (ruleMatches part) rules)
+
+getDestination :: Rule -> String
+getDestination (Rule _ _ _ destination) = destination
+
+ruleMatches :: Part -> Rule -> Bool
+ruleMatches part (Rule property operator value _) = operator (part M.! property) value
 
 ratings :: M.Map k Int -> Int
 ratings = sum . M.elems
 
 solve2 :: Input -> Int
-solve2 = countPaths "in" [] . getWorkflows
+solve2 = countPaths "in" initialPaths . getWorkflows
+
+initialPaths :: M.Map Char [Int]
+initialPaths = M.fromList $ map (\c -> (c, [1..4000])) "xmas"
 
 getWorkflows :: Input -> M.Map String Workflow
 getWorkflows (Input workflows _) = workflows
 
-countPaths :: String -> [Rule] -> M.Map String Workflow -> Int
-countPaths "A" paths _ = comboCount paths
-countPaths name paths workflows =
-    case M.lookup name workflows of
-        Nothing -> 0
-        Just workflow -> workflowPaths workflows paths workflow
+countPaths :: String -> M.Map Char [Int] -> M.Map String Workflow -> Int
+countPaths "A" paths _ = product (map length (M.elems paths))
+countPaths name paths workflows = maybe 0 (workflowPaths workflows paths) (M.lookup name workflows)
 
-workflowPaths :: M.Map String Workflow -> [Rule] -> Workflow -> Int
-workflowPaths workflows paths (Workflow [] defaultDestination) =
-    countPaths defaultDestination paths workflows
+workflowPaths :: M.Map String Workflow -> M.Map Char [Int] -> Workflow -> Int
+workflowPaths workflows paths (Workflow [] defaultDestination) = countPaths defaultDestination paths workflows
 workflowPaths workflows paths (Workflow (rule@(Rule _ _ _ destination):rules) defaultDestination) =
-    countPaths destination (rule : paths) workflows +
-        workflowPaths workflows (invertRule rule : paths) (Workflow rules defaultDestination)
+    countPaths destination (applyRule rule paths) workflows +
+        workflowPaths workflows (applyRule (invertRule rule) paths) (Workflow rules defaultDestination)
+
+applyRule :: Rule -> M.Map Char [Int] -> M.Map Char [Int]
+applyRule (Rule property operator value _) = M.update (Just . applyRule' operator value) property
+
+applyRule' :: (a -> p -> Bool) -> p -> [a] -> [a]
+applyRule' operator value = filter (`operator` value)
 
 invertRule :: Rule -> Rule
-invertRule (Rule property '<' value destination) = Rule property '>' (value-1) destination
-invertRule (Rule property '>' value destination) = Rule property '<' (value+1) destination
-invertRule _ = error "bad operator"
-
-comboCount :: [Rule] -> Int
-comboCount rules = product $ map (ruleCombos rules) "xmas"
-
-ruleCombos :: [Rule] -> Char -> Int
-ruleCombos rules property = ruleCombos' $ filter (hasProperty property) rules
-
-hasProperty :: Char -> Rule -> Bool
-hasProperty property (Rule property' _ _ _) = property == property'
-
-ruleCombos' :: [Rule] -> Int
-ruleCombos' rules = length $ filter (\i -> all (passes i) rules) [1..4000]
-
-passes :: Int -> Rule -> Bool
-passes i (Rule _ '<' value _) = i < value
-passes i (Rule _ '>' value _) = i > value
-passes _ _ = error "bad operator"
+invertRule (Rule property operator value destination) = Rule property (\a b -> not (operator a b)) value destination
