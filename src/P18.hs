601123e829bb3833102aaf9fefd587fd9c835583
Jonathan Travaglia <jonathan.travaglia@gmail.com>
2023-12-22 15:22:49 +1030

    Clean day 18

--- src/P18.hs
+++ src/P18.hs
@@ -3,7 +3,8 @@ module P18 (run1, run2, inputLocation) where
 import Lib (Coord)
 import Data.Char (digitToInt)
 
-data Instruction = Instruction Char Int
+data Direction = DirUp | DirDown | DirLeft | DirRight
+data Instruction = Instruction Direction Int
 
 run1 :: String -> Int
 run1 = solve . parse
@@ -18,8 +19,15 @@ parse :: String -> [Instruction]
 parse = map (parseLine . words) . lines
 
 parseLine :: [String] -> Instruction
-parseLine (dirStr:lenStr:_) = Instruction (head dirStr) (read lenStr)
+parseLine (dirStr:lenStr:_) = Instruction (parseDirection1 dirStr) (read lenStr)
 parseLine _ = error "bad input"
+
+parseDirection1 :: String -> Direction
+parseDirection1 ('R':_) = DirRight
+parseDirection1 ('L':_) = DirLeft
+parseDirection1 ('U':_) = DirUp
+parseDirection1 ('D':_) = DirDown
+parseDirection1 _ = error "Bad direction"
 
 solve :: [Instruction] -> Int
 solve instructions = calcArea (vertices instructions) + borderDiff instructions
@@ -28,18 +36,17 @@ vertices :: [Instruction] -> [Coord]
 vertices = scanl applyInstruction (0,0)
 
 applyInstruction :: Coord -> Instruction -> Coord
-applyInstruction (x,y) (Instruction 'R' len) = (x+len, y)
-applyInstruction (x,y) (Instruction 'L' len) = (x-len, y)
-applyInstruction (x,y) (Instruction 'U' len) = (x, y-len)
-applyInstruction (x,y) (Instruction 'D' len) = (x, y+len)
-applyInstruction _ _ = error "bad direction"
+applyInstruction (x,y) (Instruction DirRight len) = (x+len, y)
+applyInstruction (x,y) (Instruction DirLeft len) = (x-len, y)
+applyInstruction (x,y) (Instruction DirUp len) = (x, y-len)
+applyInstruction (x,y) (Instruction DirDown len) = (x, y+len)
 
 calcArea :: [Coord] -> Int
 calcArea points =
     let ys = map snd points
         xs = map fst points
         xPlus = tail xs ++ [head xs]
-        xMinus = (last xs) : init xs
+        xMinus = last xs : init xs
     in  sum (map shoelaceTerm (zip3 ys xPlus xMinus)) `div` 2
 
 shoelaceTerm :: (Int, Int, Int) -> Int
@@ -50,34 +57,49 @@ parse2 = map (parseLine2 . last . words) . lines
 
 parseLine2 :: String -> Instruction
 parseLine2 s =
-    let len = parseHex (take 5 (drop 2 s))
-        dir = parseDir (s !! 7)
+    let len = parseLength2 s
+        dir = parseDirection2 (s !! 7)
     in  Instruction dir len
 
-parseHex :: String -> Int
-parseHex = foldl (\acc x -> acc * 16 + digitToInt x) 0
+parseLength2 :: String -> Int
+parseLength2 = parseHex . lengthSection
 
-parseDir :: Char -> Char
-parseDir '0' = 'R'
-parseDir '1' = 'D'
-parseDir '2' = 'L'
-parseDir '3' = 'U'
-parseDir _ = error "bad direction code"
+lengthSection :: String -> String
+lengthSection = take 5 . drop 2
+
+parseHex :: String -> Int
+parseHex = foldl (\acc -> (+ acc * 16)) 0 . map digitToInt
+
+parseDirection2 :: Char -> Direction
+parseDirection2 '0' = DirRight
+parseDirection2 '1' = DirDown
+parseDirection2 '2' = DirLeft
+parseDirection2 '3' = DirUp
+parseDirection2 _ = error "bad direction code"
 
 borderDiff :: [Instruction] -> Int
-borderDiff instructions = sum (map (\(Instruction _ len) -> len) instructions) `div` 2 + netCorners instructions `div` 4
+borderDiff instructions = totalLength instructions `div` 2 + netCorners instructions `div` 4
 
-netCorners :: Num a => [Instruction] -> a
-netCorners instructions = sum $ zipWith cornerPolarity instructions (tail instructions ++ [head instructions])
+totalLength :: [Instruction] -> Int
+totalLength = sum . map instructionLength
 
-cornerPolarity :: Num a => Instruction -> Instruction -> a
+instructionLength :: Instruction -> Int
+instructionLength (Instruction _ len) = len
+
+netCorners :: [Instruction] -> Int
+netCorners = sum . zipWithSelfOffset cornerPolarity
+
+zipWithSelfOffset :: (b -> b -> c) -> [b] -> [c]
+zipWithSelfOffset f xs = zipWith f xs (tail xs ++ [head xs])
+
+cornerPolarity :: Instruction -> Instruction -> Int
 cornerPolarity (Instruction dir1 _) (Instruction dir2 _)
     | isInsideTurn dir1 dir2 = -1
     | otherwise = 1
 
-isInsideTurn :: Char -> Char -> Bool
-isInsideTurn 'R' 'U' = True
-isInsideTurn 'U' 'L' = True
-isInsideTurn 'L' 'D' = True
-isInsideTurn 'D' 'R' = True
+isInsideTurn :: Direction -> Direction -> Bool
+isInsideTurn DirRight DirUp = True
+isInsideTurn DirUp DirLeft = True
+isInsideTurn DirLeft DirDown = True
+isInsideTurn DirDown DirRight = True
 isInsideTurn _ _ = False
