fe98d9de6adb6e5eb974f2dc9b5a9823bb3fb862
Jonathan Travaglia <jonathan.travaglia@gmail.com>
2023-12-22 15:02:13 +1030

    Clean day 17

--- src/P17.hs
+++ src/P17.hs
@@ -6,13 +6,15 @@ import Lib ( textToCoordMap, Coord )
 import qualified Data.Map as M
 import qualified Data.Set as S
 import Data.List (minimumBy)
-import Data.Maybe (catMaybes)
 import Data.Char (digitToInt)
 import Data.Function (on)
 
 data Orientation = Horizontal | Vertical deriving (Ord, Eq)
 data Direction = DirLeft | DirRight | DirUp | DirDown deriving (Ord, Eq)
-data CurrentNode = CurrentNode Coord Direction Int Int deriving Eq -- location direction momentum heat
+type Node = (Coord, Orientation)
+type Unvisited = S.Set Node
+type Reachable = M.Map Node Int
+type Input = M.Map Coord Int
 
 run1 :: String -> Int
 run1 = solve1 . parse
@@ -23,75 +25,95 @@ run2 = solve2 . parse
 inputLocation :: String
 inputLocation = "inputs/input17"
 
-instance Ord CurrentNode where
-    compare (CurrentNode _ _ _ heat1) (CurrentNode _ _ _ heat2) = compare heat1 heat2
-
-parse :: String -> M.Map Coord Int
+parse :: String -> Input
 parse =  M.map digitToInt . textToCoordMap
 
-solve1 :: M.Map Coord Int -> Int
-solve1 input = dijkstra17 input 1 3 (targetCoord input) (initState input) (M.fromList [(((0,0), Vertical), 0), (((0,0), Horizontal), 0)]) ((0,0), Vertical, 0)
+solve1 :: Input -> Int
+solve1 input = dijkstra17 input 0 3 (targetCoord input) (initState input) initialReachable startNode 0
 
-solve2 :: M.Map Coord Int -> Int
-solve2 input = dijkstra17 input 4 7 (targetCoord input) (initState input) (M.fromList [(((0,0), Vertical), 0), (((0,0), Horizontal), 0)]) ((0,0), Vertical, 0)
+solve2 :: Input -> Int
+solve2 input = dijkstra17 input 3 7 (targetCoord input) (initState input) initialReachable startNode 0
 
-targetCoord :: M.Map Coord Int -> Coord
-targetCoord input = (maximum (map fst (M.keys input)), maximum (map snd (M.keys input)))
+initialReachable :: Reachable
+initialReachable = M.fromList [(((0,0), Vertical), 0), (((0,0), Horizontal), 0)]
 
-initState :: M.Map Coord Int -> S.Set (Coord, Orientation)
-initState = S.fromList . concatMap (\loc -> [(loc, orientation) | orientation <- [Horizontal, Vertical]]) . M.keys . M.delete (0,0)
+startNode :: Node
+startNode = ((0,0), Vertical)
 
-dijkstra17 :: M.Map Coord Int -> Int -> Int -> Coord -> S.Set (Coord, Orientation) -> M.Map (Coord, Orientation) Int -> (Coord, Orientation, Int) -> Int
-dijkstra17 heatMap minMomentum maxMove target unvisited reachable (location, orientation, heat)
+targetCoord :: Input -> Coord
+targetCoord input = (maxCoord fst input, maxCoord snd input)
+
+maxCoord :: (Coord -> Int) -> Input -> Int
+maxCoord f = maximum . map f . M.keys
+
+initState :: Input -> Unvisited
+initState = S.fromList . concatMap locationToNodes . M.keys . M.delete (0,0)
+
+locationToNodes :: Coord -> [(Coord, Orientation)]
+locationToNodes loc = [(loc, orientation) | orientation <- [Horizontal, Vertical]]
+
+dijkstra17 :: Input -> Int -> Int -> Coord -> Unvisited -> Reachable -> Node -> Int -> Int
+dijkstra17 heatMap minMomentum maxMove target unvisited reachable node@(location, _) heat
     | location == target = heat
     | otherwise =
-        let directions = turnDirs orientation
-            toUpdate = filter (isUnvisited unvisited reachable) $ catMaybes $ concatMap (\direction' -> nextBlocks minMomentum maxMove heatMap location direction' heat) directions
-            reachable' = updateNeighbours reachable toUpdate
-            reachable'' = M.delete (location, orientation) reachable'
+        let toUpdate = nodesToUpdate heatMap minMomentum maxMove unvisited reachable node heat
+            reachable' = updateReachable reachable node toUpdate
             unvisited' = markVisited unvisited toUpdate
-        in  dijkstra17 heatMap minMomentum maxMove target unvisited' reachable'' (getNextNode reachable'')
+            (node', heat') = getNextNode reachable'
+        in  dijkstra17 heatMap minMomentum maxMove target unvisited' reachable' node' heat'
 
-isUnvisited :: S.Set (Coord, Orientation) -> M.Map (Coord, Orientation) a -> (Coord, Orientation, c) -> Bool
-isUnvisited unvisited reachable (coord, orientation, _) = S.member node unvisited || M.member node reachable
-    where node = (coord, orientation)
+nodesToUpdate :: Input -> Int -> Int -> Unvisited -> Reachable -> Node -> Int -> [(Node, Int)]
+nodesToUpdate heatMap minMomentum maxMove unvisited reachable node = filter (isUnvisited unvisited reachable) . nodesToUpdate' heatMap minMomentum maxMove node
 
-getNextNode :: M.Map (a, b) Int -> (a, b, Int)
-getNextNode = toNode . minimumBy (compare `on` snd) . M.toList
+nodesToUpdate' :: Input -> Int -> Int -> Node -> Int -> [(Node, Int)]
+nodesToUpdate' heatMap minMomentum maxMove (location, orientation) heat = concatMap (nodesToUpdateInDirection minMomentum maxMove heatMap location heat) (turnDirs orientation)
 
-toNode :: ((a, b), c) -> (a, b, c)
-toNode ((location, direction), heat) = (location, direction, heat)
+nodesToUpdateInDirection :: Int -> Int -> Input -> Coord -> Int -> Direction -> [(Node, Int)]
+nodesToUpdateInDirection minMomentum maxMove heatMap location heat direction = nextBlocks heatMap minMomentum maxMove location direction heat
 
-markVisited :: (Ord a, Ord b) => S.Set (a, b) -> [(a, b, c)] -> S.Set (a, b)
-markVisited state = foldl (flip S.delete) state . map (\(loc, orientation, _) -> (loc, orientation))
+updateReachable :: Reachable -> Node -> [(Node, Int)] -> Reachable
+updateReachable reachable node = M.delete node . updateNeighbours reachable
 
-updateNeighbours :: M.Map (Coord, Orientation) Int -> [(Coord, Orientation, Int)] -> M.Map (Coord, Orientation) Int
+isUnvisited :: S.Set (Coord, Orientation) -> M.Map (Coord, Orientation) a -> (Node, c) -> Bool
+isUnvisited unvisited reachable (node, _) = S.member node unvisited || M.member node reachable
+
+getNextNode :: M.Map Node Int -> (Node, Int)
+getNextNode = minimumBy (compare `on` snd) . M.toList
+
+markVisited ::Unvisited -> [(Node, a)] -> Unvisited
+markVisited state = foldl (flip S.delete) state . map fst
+
+updateNeighbours :: M.Map Node Int -> [(Node, Int)] -> M.Map Node Int
 updateNeighbours = foldl updateNeighbour
 
-updateNeighbour :: M.Map (Coord, Orientation) Int -> (Coord, Orientation, Int) -> M.Map (Coord, Orientation) Int
-updateNeighbour state (location, orientation, heat) = M.insertWith min (location, orientation) heat state
+updateNeighbour :: M.Map Node Int -> (Node, Int) -> M.Map Node Int
+updateNeighbour state (node, heat) = M.insertWith min node heat state
 
-move :: (Num b, Num a) => Direction -> (a, b) -> (a, b)
+move :: Direction -> Coord -> Coord
 move DirDown (x,y) = (x, y+1)
 move DirUp (x,y) = (x, y-1)
 move DirLeft (x,y) = (x-1, y)
 move DirRight (x,y) = (x+1, y)
 
-step :: (Ord a, Ord b, Num b, Num a, Num c) => M.Map (a, b) c -> Maybe ((a, b), Direction, c) -> Maybe ((a, b), Direction, c)
-step _ Nothing = Nothing
-step heatMap (Just (loc, direction, heat)) =
-    let loc' = move direction loc
-    in  case M.lookup loc' heatMap of
-            Nothing -> Nothing
-            Just newHeat ->
-                let heat' = heat + newHeat
-                in  Just (loc', direction, heat')
+nextBlocks :: Input -> Int -> Int -> Coord -> Direction -> Int -> [(Node, Int)]
+nextBlocks input minMomentum maxMove start direction initHeat =
+    let locations = moveContinuous direction start
+        orientation = dirOrientation direction
+    in  validStops minMomentum maxMove $ addHeat input orientation initHeat locations
 
-nextBlocks :: (Ord a, Ord b, Num b, Num a, Num c) => Int -> Int -> M.Map (a, b) c -> (a, b) -> Direction -> c -> [Maybe ((a, b), Orientation, c)]
-nextBlocks minMomentum maxMove heatMap location direction startHeat = map convertOrientation $ take maxMove $ drop minMomentum $ iterate (step heatMap) (Just (location, direction, startHeat))
+addHeat :: Input -> Orientation -> Int -> [Coord] -> [(Node, Int)]
+addHeat _ _ _ [] = []
+addHeat input orientation heatTotal (location:locations) = case M.lookup location input of
+    Nothing -> []
+    Just heat -> (node, heatTotal') : addHeat input orientation heatTotal' locations
+        where heatTotal' = heat + heatTotal
+              node = (location, orientation)
 
-convertOrientation :: Maybe (a, Direction, c) -> Maybe (a, Orientation, c)
-convertOrientation = fmap (\(c, dir, heat) -> (c, dirOrientation dir, heat))
+moveContinuous :: Direction -> Coord -> [Coord]
+moveContinuous direction = tail . iterate (move direction)
+
+validStops :: Int -> Int -> [a] -> [a]
+validStops minMomentum maxMove = take maxMove . drop minMomentum
 
 turnDirs :: Orientation -> [Direction]
 turnDirs Horizontal = [DirLeft, DirRight]
