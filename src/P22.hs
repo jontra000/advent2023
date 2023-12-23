c2a5ec50eab78e39abb5d73d90523d8433899701
Jonathan Travaglia <jonathan.travaglia@gmail.com>
2023-12-23 13:42:51 +1030

    Day 22 performance improvements

--- src/P22.hs
+++ src/P22.hs
@@ -4,7 +4,10 @@ module P22 (run1, run2, inputLocation) where
 import Data.List.Split (splitOn)
 
 import qualified Data.Set as S
+import qualified Data.Map as M
 import Data.List (sortOn, delete)
+import Data.Maybe (fromMaybe)
+import Lib (Coord)
 
 type Coord3 = (Int, Int, Int)
 
@@ -17,14 +20,14 @@ run2 = solve2 . parse
 inputLocation :: String
 inputLocation = "inputs/input22"
 
-solve1 :: [[Coord3]] -> Int
-solve1 = disintegrationCount . dropBricks . sortBricks
+solve1 :: [S.Set Coord3] -> Int
+solve1 = length . filter (==0) . fallCount M.empty
 
-solve2 :: [[Coord3]] -> Int
-solve2 = fallCount . dropBricks . sortBricks
+solve2 :: [S.Set Coord3] -> Int
+solve2 = sum . fallCount M.empty
 
-parse :: String -> [[Coord3]]
-parse = map parseLine . lines
+parse :: String -> [S.Set Coord3]
+parse = dropBricks . sortBricks . map parseLine . lines
 
 parseLine :: String -> [Coord3]
 parseLine = parseBrick . map parseCoord . splitOn "~"
@@ -41,36 +44,47 @@ parseBrick [a@[x1,y1,z1], b@[x2,y2,z2]]
     | otherwise = error ("non linear brick: " ++ show a ++ " " ++ show b) 
 parseBrick _ = error "bad brick input"
 
-dropBricks :: [[Coord3]] -> [[Coord3]]
-dropBricks bricks =
-    let bricks' = dropBricks' S.empty bricks
-    in  if bricks'== bricks
-        then bricks
-        else dropBricks bricks'
+dropBricks :: [S.Set Coord3] -> [S.Set Coord3]
+dropBricks = dropBricks' M.empty
 
-dropBricks' :: S.Set Coord3 -> [[Coord3]] -> [[Coord3]]
+dropBricks' :: M.Map Coord Int -> [S.Set Coord3] -> [S.Set Coord3]
 dropBricks' _ [] = []
-dropBricks' laidBricks (next:bricks) =
-    toAdd : dropBricks' (S.union laidBricks (S.fromList toAdd)) bricks
-        where next' = map (\(x,y,z) -> (x,y,z-1)) next
-              toAdd = if any ((<1) . (\(_,_,z) -> z)) next' || not (S.null (S.intersection laidBricks (S.fromList next'))) then next else next'
+dropBricks' laidBricks (next:bricks) = next' : dropBricks' laidBricks' bricks
+        where next' = dropBrick laidBricks next
+              laidBricks' = updateLaidBricks laidBricks next'
 
-sortBricks :: [[Coord3]] -> [[Coord3]]
-sortBricks = sortOn (minimum . map (\(_,_,z) -> z))
+updateLaidBricks :: M.Map Coord Int -> S.Set Coord3 -> M.Map Coord Int
+updateLaidBricks = foldl (\state (x,y,z) -> M.insertWith max (x,y) z state)
 
-disintegrationCount :: [[Coord3]] -> Int
+dropBrick :: M.Map Coord Int -> S.Set Coord3 -> S.Set Coord3
+dropBrick laidBricks brick = dropToZ brick $ (+1) $ topZ laidBricks $ horizontalCoords brick
+
+horizontalCoords :: S.Set Coord3 -> S.Set Coord
+horizontalCoords = S.map (\(x,y,_) -> (x,y))
+
+topZ :: M.Map Coord Int -> S.Set Coord -> Int
+topZ laidBricks horizontal = fromMaybe 0 (S.lookupMax (S.map (fromMaybe 0 . (`M.lookup` laidBricks)) horizontal))
+
+dropToZ :: S.Set Coord3 -> Int -> S.Set Coord3
+dropToZ brick z' =
+    let zDiff = z' - minimum (S.map (\(_,_,z) -> z) brick)
+    in  S.map (\(x,y,z) -> (x,y,z+zDiff)) brick
+
+sortBricks :: [[Coord3]] -> [S.Set Coord3]
+sortBricks = map S.fromList . sortOn (minimum . map (\(_,_,z) -> z))
+
+disintegrationCount :: [S.Set Coord3] -> Int
 disintegrationCount bricks = length $ filter (canDisintegrate bricks) bricks
 
-canDisintegrate :: [[Coord3]] -> [Coord3] -> Bool
+canDisintegrate :: [S.Set Coord3] -> S.Set Coord3 -> Bool
 canDisintegrate bricks brick =
     let bricks' = delete brick bricks
-    in  bricks' == dropBricks' S.empty bricks' 
+    in  bricks' == dropBricks bricks' 
 
-fallCount :: [[Coord3]] -> Int
-fallCount bricks = sum $ map (fallCount' bricks) bricks
-
-fallCount' :: [[Coord3]] -> [Coord3] -> Int
-fallCount' bricks brick =
-    let bricks' = delete brick bricks
-        bricks'' = dropBricks' S.empty bricks'
-    in  length $ filter not $ zipWith (==) bricks' bricks''
+fallCount :: M.Map Coord Int -> [S.Set Coord3] -> [Int]
+fallCount _ [] = []
+fallCount laid (nextBrick:bricks) =
+    let bricks' = dropBricks' laid bricks
+        droppedCount = length $ filter not $ zipWith (==) bricks bricks'
+        laid' = updateLaidBricks laid nextBrick
+    in  droppedCount : fallCount laid' bricks
